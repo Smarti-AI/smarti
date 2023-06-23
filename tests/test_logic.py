82f0dc423ea38b36adad1bbec1f431aa7464301a
dimastatz <dima.statz@gmail.com>
2023-06-23 13:35:50 +0300

    add test

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,15 +1,23 @@
 """test logic"""
 
+import os
 import smarti.logic.facade as pf
 
 
 def test_index(flask_app, client):
-    """test workbook upload"""
+    """test flask_app default"""
     assert flask_app is not None
     res = client.get("/")
     assert res.status_code == 200
     content = res.get_data(as_text=True)
     assert "Smarti" in content
+
+
+def test_heroku_boot():
+    """test content of boot file"""
+    with open("./Procfile") as f:
+        line = f.readlines()[0]
+        assert "smarti.app" in line
 
 
 def test_upload_workbook():
