3868da6398356b07bc00390b638854616aaf8c60
dimastatz <dima.statz@gmail.com>
2023-06-30 09:56:50 +0300

    refactor easy OCR

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,12 +1,20 @@
 """test OCR reader"""
 
-import smarti.ocr.text_reader as ocr
+import smarti.ocr.ocr_easyocr as ocr
 
 
 def test_text_reader(input_folder):
     """test image to OCR translate"""
-    fname = input_folder + "formula1.png"
-    with open(fname, "rb") as file:
-        content = file.read()
-        res = ocr.read(content)
-        assert not res is None
+    results = []
+
+    ocr.easyocr_reader = ocr.recreate_reader(["en"])
+
+    with open(input_folder + "formula1.png", "rb") as file:
+        results.append(ocr.read_text(file.read(), ocr.easyocr_reader))
+        print(results[-1])
+
+    with open(input_folder + "question.png", "rb") as file:
+        results.append(ocr.read_text(file.read(), ocr.easyocr_reader))
+        print(results[-1])
+
+    assert len(results) == 2
